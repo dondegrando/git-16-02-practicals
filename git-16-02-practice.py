2914f209453c1c473e5675960e17c0308b52ddc4
Nnoko Degrando <dondegrando@gmail.com>
2021-02-16 10:19:16 +0100

    made a modification on the code
    this is to demonstrate the braching functionality of python

--- git-16-02-practice.py
+++ git-16-02-practice.py
@@ -3,4 +3,8 @@
 print("There is no rich person in the world who hasn't gone through hard times")
 for i in range(10):
 	print(i, "\t")
+	
+# this is just a change i am making locally on a feature branch 
+print("i wrote this line of code in the feature branch")
+
 
